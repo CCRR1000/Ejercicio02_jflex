f9f8d6c83565d92b183658cd1a8db23368f515c7
Michelle Cifuentes <j.micro.0101@gmail.com>
2022-02-14 13:51:10 -0600

    Ejercicio jflex

---
+++ EjecicioJFLex/src/main/java/main/Entero.java
@@ -0,0 +1,40 @@
+
+package main;
+
+public class Entero {
+    
+    private int valor, linea, columna;
+
+    public Entero(int valor, int linea, int columna) {
+        this.valor = valor;
+        this.linea = linea;
+        this.columna = columna;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public int getLinea() {
+        return linea;
+    }
+
+    public void setLinea(int linea) {
+        this.linea = linea;
+    }
+
+    public int getColumna() {
+        return columna;
+    }
+
+    public void setColumna(int columna) {
+        this.columna = columna;
+    }
+    
+    
+    
+}
